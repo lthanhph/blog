a95db5a0bdd3281b7286922f00fb8c643d08d3d3
DESKTOP-35KK89A\Phule <lthanhph@gmail.com>
2022-01-19 11:06:53 +0700

    fix asset

--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -34,7 +34,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
         <script src="{{ asset('vendor/jquery-ui-1.13.0/jquery-ui.js') }}"></script>
-        <script src="{{ secure_asset('admin_asset/js/main.js') }}"></script>
+        <script src="{{ asset('admin_asset/js/main.js') }}"></script>
     </footer>
 </body>
 
